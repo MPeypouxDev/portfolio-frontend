b29991059a3f625522406da517034828b0674b81
Mathys <M.PeypouxDev@gmail.com>
2026-02-03 15:10:47 +0100

    feat: add global footer across all pages

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import Footer from './Footer'
 
 function Layout() {
   return (
@@ -8,6 +9,7 @@ function Layout() {
       <main className="pt-16">
         <Outlet />
       </main>
+      <Footer />
     </div>
   )
 }
