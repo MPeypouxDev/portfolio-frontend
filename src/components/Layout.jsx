fe591c242f95bb914047eabd8e8643c0c4af4cc3
Mathys <M.PeypouxDev@gmail.com>
2026-02-24 14:29:13 +0100

    feat: implement route fade transition using AnimatePresence and useLocation in Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,19 +1,26 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import ScrollToTop from './ScrollToTop'
 import Background from './Background'
+import { AnimatePresence, motion } from 'framer-motion'
 
 function Layout() {
+  const location = useLocation()
+
   return (
     <>
       <Background />
       <div className="relative z-10 min-h-screen flex flex-col">
         <Navbar />
         <ScrollToTop />
-        <main className="flex-grow pt-16">
-          <Outlet />
-        </main>
+        <AnimatePresence>
+          <motion.div key={location.key} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+              <main className="flex-grow pt-16">
+                <Outlet />
+              </main>
+          </motion.div>
+        </AnimatePresence>
         <Footer />
       </div>
     </>
